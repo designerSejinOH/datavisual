be4a494c466724596eec4644c1d51e09b807dbb4
SejinOH <39869184+designerSejinOH@users.noreply.github.com>
2022-12-05 10:46:05 +0900

    modify data background

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -4,15 +4,18 @@ let ppMax;
 let ppMin;
 let covid;
 let nCovid;
+let pp;
+let inpp;
 
 $.get(
   "https://datacook.org/api/open/dishes?id=4388&token=SK6250UN3SUWvTEWKwOb81225",
   function (data, status) {
     let json = data;
-    console.log(json);
     let obj = JSON.parse(json);
     city = obj.city;
     lvl = obj.lvl;
+    pp = obj.pp;
+    inpp = obj.inpp;
     ppMax = obj.ppMax;
     ppMin = obj.ppMin;
     covid = obj.covid;
@@ -29,9 +32,9 @@ function draw() {
   let fontSize = 20;
   let lineHeight = 10;
   textSize(fontSize);
-  fill(0, 102, 153, 255);
+  fill(0);
   text(" 도시 : " + city, lineHeight, fontSize + lineHeight);
-  fill(0, 102, 153, 200);
+  fill(0, 102, 153, 255);
   text(" 인구밀집 : " + lvl, lineHeight, (fontSize + lineHeight) * 2);
   fill(0, 102, 153, 180);
   text(" 실시간인구최대 : " + ppMax, lineHeight, (fontSize + lineHeight) * 3);
@@ -44,6 +47,11 @@ function draw() {
     " 코로나추가확진자 : " + nCovid,
     lineHeight,
     (fontSize + lineHeight) * 6
+  );
+  text(
+    " 성동구소재지 생활인구 : " + inpp,
+    lineHeight,
+    (fontSize + lineHeight) * 7
   );
 }
 
